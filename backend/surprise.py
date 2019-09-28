7d728fe8b1b0247653aa28fd9e0af2613e6bb2e8
Alexander Davis <alexanderdavis.net@googlemail.com>
2019-09-28 04:10:40 +0200

    create surprise.py

---
+++ backend/surprise.py
@@ -0,0 +1,30 @@
+from flask import request
+
+# Module: surorise.py
+
+
+def main():
+    assert request.method == 'POST'
+    data = request.get_json()
+
+    missing = checkJson(expectedFields, data)
+    if missing:
+        print('Missing field: %s' % missing)
+        return 'Missing field: %s' % missing
+
+    print(data)
+    return "surprise"
+
+
+def checkJson(expectedFields, data):
+    if not data:
+        return 'data'
+
+    for f in expectedFields:
+        try:
+            data[f]
+        except:
+            return f
+
+
+expectedFields = ['time', 'location', 'categories']
