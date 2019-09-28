48584f14145cbb5a66aca8c9df9df553081b18ee
jaysuz <jonas.augsburger@gmail.com>
2019-09-28 14:16:45 +0200

    Add material resources

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
+
 Vue.config.productionTip = false
 
 new Vue({
