e67c79c3ad36afec02546192bc17456ecf255973
Stefanie Fessler <stefanie.fessler@swisscom.com>
2019-09-29 00:25:27 +0200

    Add home button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,50 @@
 <template>
-  <md-app id="app">
-    <md-app-toolbar class="md-primary">
-      <span class="md-title">SBB Surprise</span>
-    </md-app-toolbar>
-    <md-app-content>
-      <router-view/>
-    </md-app-content>
-  </md-app>
+    <md-app id="app">
+        <md-app-toolbar class="md-primary">
+            <button id="homebutton" class="md-title" @click="goHome">SBB Surprise</button>
+        </md-app-toolbar>
+        <md-app-content>
+            <router-view/>
+        </md-app-content>
+    </md-app>
 </template>
 
+<script>
+    import router from "./router";
+
+    export default {
+        methods: {
+            goHome() {
+                router.push({name: 'home'})
+            }
+        }
+    }
+</script>
+
 <style lang="scss">
-  @import "~vue-material/dist/theme/engine"; // Import the theme engine
+    @import "~vue-material/dist/theme/engine"; // Import the theme engine
 
-  @include md-register-theme("default", (
-  primary: md-get-palette-color(red, 900), // The primary color of your application
-  accent: md-get-palette-color(blue, 200) // The accent or secondary color
-  ));
+    @include md-register-theme("default", (
+            primary: md-get-palette-color(red, 900), // The primary color of your application
+            accent: md-get-palette-color(blue, 200) // The accent or secondary color
+    ));
 
-  @import "~vue-material/dist/theme/all"; // Apply the theme
+    @import "~vue-material/dist/theme/all"; // Apply the theme
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    #homebutton {
+        background: none;
+        border: none;
+    }
+
+    #homebutton:hover {
+        cursor: pointer;
+    }
 </style>
