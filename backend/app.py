2412af614b85fe59c285b5c9fe4ba397c7c0eb10
Alexander Davis <alexanderdavis.net@googlemail.com>
2019-09-28 04:10:40 +0200

    create surprise.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+import surprise
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/categories')
+def categories():
+    return ['mountain']
+
+
+@app.route('/surprise', methods=['POST'])
+def surprisePost():
+    return surprise.main()
